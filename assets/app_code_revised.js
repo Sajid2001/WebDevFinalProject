bb2e812d158165253be28a14be251fdfc87d8070
Sajid Syed <sajidsyed201@gmail.com>
2021-12-09 20:46:05 -0500

    It it fixed

--- assets/app_code_revised.js
+++ assets/app_code_revised.js
@@ -4,11 +4,11 @@ var updateView = async(button) => {
     case 'by_instructor':
       console.log(button.dataset.querytype);
       let queryName = document.querySelector("#instructorQuery").value;
-      api = `http://localhost:5000/api/by_instructor/${queryName}`;
+      api = `https://course-query-project.herokuapp.com/api/by_instructor/${queryName}`;
       break;
     case 'by_code':
       let queryCode = document.querySelector("#codeQuery").value;
-      api = `http://localhost:5000/api/by_course_code/${queryCode}`;
+      api = `https://course-query-project.herokuapp.com/api/by_course_code/${queryCode}`;
       break;
     case 'by_level':
       console.log(button.dataset.querytype);
@@ -19,7 +19,7 @@ var updateView = async(button) => {
                 queryLevel = choice.value;
               }
             }
-      api = `http://localhost:5000/api/by_level/${queryLevel}`;
+      api = `https://course-query-project.herokuapp.com/api/by_level/${queryLevel}`;
       break;
     case 'dual_query':
       let dual_queryName = document.querySelector("#dual_name").value;
@@ -34,7 +34,7 @@ var updateView = async(button) => {
                   break;
                 }
               }
-              api = `http://localhost:5000/api/combined_query/${dual_queryName}/${dual_queryLevel}`;
+              api = `https://course-query-project.herokuapp.com/api/combined_query/${dual_queryName}/${dual_queryLevel}`;
         break;
 
   }
